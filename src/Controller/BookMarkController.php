4ea8e3577afc9682218b60ccb025e74a3995df03
Isaia91 <isaia.maperi@gmail.com>
2025-02-20 14:58:22 +1100

    function to add bookmarks on url access added on bookmark controller

--- src/Controller/BookMarkController.php
+++ src/Controller/BookMarkController.php
@@ -11,14 +11,43 @@ use Doctrine\ORM\EntityManagerInterface;
 
 final class BookMarkController extends AbstractController
 {
-    #[Route('/book/mark', name: 'app_book_mark')]
+
     /*public function index(): Response
     {
         return $this->render('book_mark/index.html.twig', [
             'controller_name' => 'BookMarkController',
         ]);
     }*/
+    #[Route("/book/mark/add", name: "add_bookmark")]
+    public function add_bookmark(EntityManagerInterface $entityManager): Response
+    {
+        // Les données à ajouter
+        $data = [
+            ['url' => 'https://symfony.com/', 'commentaire' => 'lien du site symfony'],
+            ['url' => 'https://www.qwant.com/?l=fr', 'commentaire' => 'lien du site vers qwant'],
+            ['url' => 'https://www.youtube.com/', 'commentaire' => 'lien du site youtube'],
+        ];
 
+        foreach ($data as $item) {
+            $bookmark = new BookMark();
+            $bookmark->setUrl($item['url']);
+            $bookmark->setCommentaire($item['commentaire']);
+            $bookmark->setDateCreation(new \DateTime());
+
+
+            $entityManager->persist($bookmark);
+        }
+
+
+        //on ajoute lorsque que l'on accede a la page
+        $entityManager->flush();
+
+        //redirection vers la page des bookmarks
+        return $this->redirectToRoute('app_book_mark');
+    }
+
+
+    #[Route('/book/mark', name: 'app_book_mark')]
     public function index(BookMarkRepository $bookMarkRepository): Response
     {
         $bookmarks = $bookMarkRepository->findAll();
